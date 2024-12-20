a35ec85a29a10502d5327b28fa0958c5e656afa2
Adam Bardsley <adam.bardsley@gmail.com>
2024-12-20 17:45:57 +0000

    Setup of basics

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 
 export default {
   content: [
@@ -11,6 +13,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
     },
   },
